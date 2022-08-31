80383703520885ca06379563a1a803b5e5290057
Tor Idland <tor.idland@nav.no>
2022-08-31 15:23:09 +0200

    ny template /soknad

--- run_development.sh
+++ run_development.sh
@@ -4,10 +4,10 @@ CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 
 docker pull navikt/pdfgen
 docker run \
-        -v $CURRENT_PATH/templates:/app/templates \
-        -v $CURRENT_PATH/fonts:/app/fonts \
-        -v $CURRENT_PATH/data:/app/data \
-        -v $CURRENT_PATH/resources:/app/resources \
+        -v "$CURRENT_PATH"/templates:/app/templates \
+        -v "$CURRENT_PATH"/fonts:/app/fonts \
+        -v "$CURRENT_PATH"/data:/app/data \
+        -v "$CURRENT_PATH"/resources:/app/resources \
         -p 8080:8080 \
         -e DISABLE_PDF_GET=false \
         -it \
